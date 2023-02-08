f3425320845850ceb9b5f235cc8881a3e6f231de
Andrey Mozerov <andrey-mozerov@mail.ru>
2023-02-08 23:17:08 +0300

    v2

--- src/test/java/RestApiTests.java
+++ src/test/java/RestApiTests.java
@@ -1,9 +1,26 @@
+
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
 public class RestApiTests {
+
+    @Test
+   void successfulLoginUserTest() {
+        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .statusCode(200)
+                .body("token", notNullValue());
+    }
+
     @Test
     void singleResourceTest() {
         given()
@@ -17,4 +34,42 @@ public class RestApiTests {
                 .statusCode(200)
                 .body("data.color", is("#C74375"));
     }
+
+    @Test
+    void getUserIdNotFound(){
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users/888")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(404);
+    }
+
+    @Test
+    void singleUserTest() {
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .when()
+                .get("https://reqres.in/api/users/3")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.email", is("emma.wong@reqres.in"));
+    }
+    @Test
+    void deleteUserTest() {
+        given()
+                .log().uri()
+                .when()
+                .delete("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(204);
+    }
 }
+
